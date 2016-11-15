b3d13ec064bafbd5047dc88926cbfe9c924a8ba4
Jim Hester <james.f.hester@gmail.com>
2016-11-15 11:21:40 -0500

    Add documentation for highlight_string

--- R/package.R
+++ R/package.R
@@ -14,6 +14,31 @@ choices <- function(path, default) {
   remove_extension(dir(path))
 }
 
+# Format the choices for display in Rd
+choices_rd <- function(x) {
+  paste0(collapse = ", ", paste0("\\sQuote{", x, "}"))
+}
+
+#' Highlight a character vector
+#'
+#' This function uses Andre Simon's
+#' \href{http://andre-simon.de/doku/highlight/en/highlight.php}{highlight}
+#' package to highlight code in
+#' \Sexpr[stage=render]{length(highlite:::choices(highlite:::package_file("langDefs")))} programming
+#' languages, markup languages and configuration files. It Can output to a variety
+#' of formats including \sQuote{HTML}, \sQuote{ANSI escape codes},
+#' \sQuote{TeX}, \sQuote{LaTeX} and \sQuote{SVG}.
+#' @param code \code{[character()]}\cr The code to highlight. Multiple lines will be joined by newlines.
+#' @param language \code{[character(1)]} default: \sQuote{r}\cr The language,
+#' one of \Sexpr[stage=render, results=rd]{highlite:::choices_rd(highlite:::choices(highlite:::package_file("langDefs")))} with
+#' the default \code{language_path}.
+#' @param output \code{[character(1)]} default: \sQuote{ESC_ANSI}\cr The output type, one of
+#' \Sexpr[stage=render, results=rd]{highlite:::choices_rd(names(highlite:::output_types()))}.
+#' @param theme \code{[character(1)]} default: \sQuote{solarized-light}\cr The theme, one of
+#' \Sexpr[stage=render, results=rd]{highlite:::choices_rd(highlite:::choices(highlite:::package_file("themes")))} with the default
+#' \code{theme_path}.
+#' @param theme_path \code{[character(1)]}\cr The directory containing \href{http://andre-simon.de/doku/highlight/en/highlight.php}{highlight} themes.
+#' @param language_path \code{[character(1)]}\cr The directory containing \href{http://andre-simon.de/doku/highlight/en/highlight.php}{highlight} language definitions.
 #' @export
 highlight_string <- function(code, language = "r", output = "ESC_ANSI", theme = "solarized-light", theme_path = package_file("themes"), language_path = package_file("langDefs")) {
   output_types <- output_types()
